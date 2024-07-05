39e52a029f5cc7007d4adccc794ff755bf190cd7
Bobby Steckline <rsteckline@gmail.com>
2024-07-05 08:38:14 -0400

    Construct timeline

---
+++ src/data/timeline.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Freelance Software Developer',
+        duration: 'March 2024 - Present',
+        details: 'Currently engineering multiple innovative projects: crafting a dynamic portfolio website using React, Vite, and Tailwind, with plans to integrate TypeScript; collaborating with diverse business owners and artists to elevate their online presence and e-commerce platforms; and advancing workflow automation for Mental Health America, significantly enhancing data management for warm-line operators.',
+    },
+    {
+        year: '2023',
+        title: 'Accredited Certificate in Software Engineering',
+        duration: 'June 2023 - March 2024',
+        details: 'As a front end engineering graduate at Turing School of Software and Design, a nonprofit and the only ACCET-accredited coding school, I built complex, user-accessible software applications as part of an Agile developer team. I consistently applied modern software engineering processes, such as test-driven development, rapid prototyping, and rapid iteration to create reliable and intuitive client-side applications. With a strong foundation in both functional and object-oriented programming, I used an array of popular tooling and frameworks, including JavaScript, HTML, CSS, React, TypeScript, TailwindCSS, Node.js, Context API, Webpack, and NPM. I modeled business data into structures viable for web applications and coordinated with back-end services and data stores to save and retrieve that data.',
+    },
+    {
+        year: '2021',
+        title: 'Executive Pastry Chef at Historic Hotels Inc.',
+        duration: 'July 2021 - February 2023',
+        details: 'Leading the pastry department at a renowned, historic venue, I managed operations across nearly 200 rooms, multiple dining areas, and various social events, integrating detailed culinary audits for event and revenue management. In a demanding environment characterized by tight deadlines and continuous activity, I adeptly balanced a multitude of responsibilities. My role encompassed the oversight of daily operations within a fast-paced, high-volume setting, requiring a steadfast commitment to excellence and efficiency. This challenging context underscored my ability to maintain high standards of culinary excellence and operational effectiveness, showcasing resilience and skill in managing complexity.',
+    },
+    {
+        year: '2019',
+        title: 'Executive Pastry Chef at the Chicago Athletic Association',
+        duration: 'July 2019 - May 2020',
+        details: 'As Executive Pastry Chef at the renowned CAA hotel in the heart of Chicago, I led a team of seven talented chefs, guiding and nurturing our pastry department in a range of pastry techniques. In this role, blending culinary artistry with strategic planning was essential, especially in such a lively and high-profile setting. This position was more than just managing kitchen operations for the restaurant, special events, programming events, and service for 240 rooms; it was about unifying a team under a shared vision of delivering outstanding and customized culinary experiences for every occasion. Throughout my tenure, I was dedicated to achieving culinary brilliance, fostering team unity, cost-efficiency, and ensuring the utmost in guest satisfaction.',
+    },
+    {
+        year: '2018',
+        title: 'Pastry Sous Chef at the Chicago Athletic Association',
+        duration: 'October 2018 - July 2019',
+        details: 'Trained under Executive Pastry Chef and World Chocolate Master Jove Hubbard.',
+    },
+    {
+        year: '2013',
+        title: 'Executive Pastry Chef at the Art Institute of Chicago for Bon Appétit Management Company',
+        duration: 'August 2013 - October 2018',
+        details: 'While overseeing the Pastry Department at the Art Institute of Chicago, I led a team of ten pastry chefs providing offerings for over 1,200 annual events which helped generate $10M in revenue yearly. This role blended meticulous planning with innovative problem-solving, requiring efficient resource management to handle complex logistics and tight deadlines. I particularly valued collaborating with culturally diverse chefs in Chicago and during my travels opening new accounts, which enriched our menu development and event execution. This hands-on experience in managing and coordinating a dynamic team not only enhanced the guest experience but also significantly contributed to the success of each event. My duration at the Art Institute was a rewarding journey in team leadership, project management, and creative problem-solving, consistently aiming for excellence in every aspect.',
+    },
+]
