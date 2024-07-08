eb6ea5144929db5d0681c063a5e6fcb73d59ce09
Bobby Steckline <rsteckline@gmail.com>
2024-07-08 19:50:14 -0400

    Add basic footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <div>Footer</div>
-    )
+  return (
+    <div className="py-5 text-center">
+      {/* Social Icons */}
+      <p className="text-sm mt-2 opacity-50 ">
+        Bobby Steckline ||
+        <span className="relative inline-block mx-2">
+          <a href="https://www.github.com/rsteckline">
+            github.com/rsteckline</a>
+        </span>
+        || 773-789-7696
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
