9882b4cebbd51297253c51ccb1f4b6930c33ed86
Bobby Steckline <rsteckline@gmail.com>
2024-07-08 19:53:10 -0400

    Style github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,10 @@ function Footer() {
       <p className="text-sm mt-2 opacity-50 ">
         Bobby Steckline ||
         <span className="relative inline-block mx-2">
-          <a href="https://www.github.com/rsteckline">
-            github.com/rsteckline</a>
+          <a href="https://www.github.com/rsteckline" className="group">
+            github.com/rsteckline
+            <span className="absolute inset-x-0 -bottom-1 h-px bg-gradient-to-r from-cyan-800/0 via-cyan-800 to-cyan-800/0 opacity-0 transition-opacity duration-200 group-hover:opacity-100"></span>
+          </a>
         </span>
         || 773-789-7696
       </p>
