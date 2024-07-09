7420d6afe7eb0546948ea510d5590980297ea3e9
Bobby Steckline <rsteckline@gmail.com>
2024-07-08 22:57:39 -0400

    Create footer and implement icon library

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div className="py-5 text-center">
-      {/* Social Icons */}
-      <p className="text-sm mt-2 opacity-50 ">
-        Bobby Steckline ||
-        <span className="relative inline-block mx-2">
-          <a href="https://www.github.com/rsteckline" className="group">
-            github.com/rsteckline
-            <span className="absolute inset-x-0 -bottom-1 h-px bg-gradient-to-r from-cyan-800/0 via-cyan-800 to-cyan-800/0 opacity-0 transition-opacity duration-200 group-hover:opacity-100"></span>
-          </a>
-        </span>
-        || 773-789-7696
-      </p>
+      <a href="https://www.linkedin.com/in/rsteckline"><i className="ci ci-linkedin ci-5x mr-6 hover:cursor-pointer"></i></a>
+      <a href="https://www.github.com/rsteckline"><i className="ci ci-github ci-5x mr-8 hover:cursor-pointer"></i></a>
+      <a href="mailto:rsteckline@gmail.com"><i className="ci ci-gmail ci-5x hover:cursor-pointer"></i></a>
+
     </div>
   );
 }
