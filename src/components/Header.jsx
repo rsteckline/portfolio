e83b9f29c26920049e271e174cbcde0373df90bc
Bobby Steckline <rsteckline@gmail.com>
2024-07-15 14:20:39 -0400

    Remove type errors and begin nav component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "./Logo";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header className="flex w-full items-center justify-between mx-auto p-8">
         <Logo />
+        <Nav />
       <h1>Header</h1>
     </header>
   );
