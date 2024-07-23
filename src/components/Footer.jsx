2f36e303856557470fbedea101f724bf7d63f0c6
Bobby Steckline <rsteckline@gmail.com>
2024-07-22 20:02:45 -0400

    Correct footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,25 @@ function Footer() {
   return (
     <div className="py-6 text-center bg-cream">
       <a
-        href="mailto:rsteckline@gmail.com"
+        href="https://calendly.com/rsteckline/30min"
         className="inline-block hover:cursor-pointer mr-8 text-main"
-      >
+        >
         <CalendarPlus size={64} className="mx-auto" />
       </a>
       <a
         href="https://www.linkedin.com/in/rsteckline"
         className="inline-block mr-7 hover:cursor-pointer text-main"
-      >
+        >
         <Linkedin size={64} className="mx-auto" />
       </a>
       <a
         href="https://www.github.com/rsteckline"
         className="inline-block mr-8 hover:cursor-pointer text-main"
-      >
+        >
         <Github size={64} className="mx-auto" />
       </a>
       <a
-        href="https://calendly.com/rsteckline/30min"
+        href="mailto:rsteckline@gmail.com"
         className="inline-block hover:cursor-pointer text-main"
       >
         <Mail size={64} className="mx-auto" />
