2e2763f0be0be2c5e820f5085df773a6681e1f5e
Bobby Steckline <rsteckline@gmail.com>
2024-07-24 12:58:13 -0400

    Add components to projects section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import portfolio from "../data/portfolio";
+import projects from "../data/projects";
 import PortfolioProject from "./PortfolioProject";
 import Title from "./Title";
 
 function Portfolio() {
   return (
-    <div className="flex flex-col items-center justify-center pt-10 pb-10 bg-cream font-jetbrains">
+    <div className="flex flex-col items-center justify-center pt-10 pb-5 bg-cream font-jetbrains">
       <div className="text-center">
         <Title>Projects</Title>
       </div>
       <div className="grid p-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {portfolio.map((project) => (
+        {projects.map((project) => (
           <PortfolioProject
             imgUrl={project.imgUrl}
             title={project.title}
---
+++ src/data/components.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: "Rating Component",
+        imgUrl: '/assets/rating.png',
+        stack: ['JavaScript', 'HTML', 'CSS'],
+        URL: "https://rating-component-fem-omega.vercel.app/",
+    },
+    {
+        title: "Summary Component",
+        imgUrl: '/assets/summary.png',
+        stack: ['HTML', 'CSS'],
+        URL: "https://rsteckline.github.io/summary-component-FEM/",
+    }
+]
