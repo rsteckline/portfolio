83254fe958ce8272ce1bccd11cd1fd4a9f07ea6a
Bobby Steckline <rsteckline@gmail.com>
2025-06-17 21:16:47 -0400

    Clarify naming conventions

---
+++ src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import projects from "../data/projects";
+import ProjectCard from "./ProjectCard";
+import Title from "./Title";
+
+function Projects() {
+  return (
+    <div className="flex flex-col items-center justify-center pt-10 pb-5 bg-cream font-jetbrains">
+      <div className="text-center">
+        <Title>Projects</Title>
+      </div>
+      <div className="grid p-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {projects.map((project) => (
+          <ProjectCard
+            imgUrl={project.imgUrl}
+            title={project.title}
+            stack={project.stack}
+            URL={project.URL}
+            key={project.title}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
